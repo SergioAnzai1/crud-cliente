0101060e25b673c48111ba45d9fa2c07b8c44068
zanzai <sergioanzai@gmail.com>
2025-11-15 17:10:42 -0300

    feat: adiciona método findByCpf com Option e query buscarPorNomeouCpf

--- src/main/java/com/empresa/crud/cliente/repository/ClienteRepository.java
+++ src/main/java/com/empresa/crud/cliente/repository/ClienteRepository.java
@@ -2,13 +2,22 @@ package com.empresa.crud.cliente.repository;
 
 import com.empresa.crud.cliente.model.Cliente;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     
-    List<Cliente> findByNomeContainingIgnoreCaseOrCpfContainingIgnoreCase(String nome, String cpf);
+    @Query("SELECT c FROM Cliente c WHERE " +
+       "LOWER(c.nome) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
+       "c.cpf LIKE CONCAT('%', :termo, '%')")
+    List<Cliente> buscarPorNomeOuCpf(@Param("termo") String termo);
+
+    Optional<Cliente> findByCpf(String cpf);
 }
